712df6c47605e52ea9ddaa96a1e558995298da87
WilAy <ayomikunakintade@gmail.com>
2024-09-02 16:09:48 +0100

    chore: removed dist

--- dist/interface/admin.interface.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AdminAccountState = void 0;
-var AdminAccountState;
-(function (AdminAccountState) {
-    AdminAccountState["ACTIVE"] = "active";
-    AdminAccountState["DISABLED"] = "inactive";
-})(AdminAccountState || (exports.AdminAccountState = AdminAccountState = {}));
-//# sourceMappingURL=admin.interface.js.map
